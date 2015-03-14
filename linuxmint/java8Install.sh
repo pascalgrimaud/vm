2626e74a14a88d3696e816c516ca944e9e1918d7
pGrimaud <pascalgrimaud@gmail.com>
2015-03-15 00:21:15 +0100

    rename the scripts

---
+++ linuxmint/java8Install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# add repository
+sudo add-apt-repository ppa:webupd8team/java -y
+
+# make sure the package repository is up to date
+sudo apt-get -y update
+
+# install Java 8
+sudo apt-get -y install oracle-java8-installer
+sudo apt-get -y install oracle-java8-set-default
+
---
+++ linuxmint/toolsInstall.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# make sure the package repository is up to date
+sudo apt-get -y update
+
+# install differents tools
+sudo apt-get -y install vim subversion curl putty
+
+
