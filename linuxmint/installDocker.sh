fde04930c78f3997b05ab6f5beb444798c90c80e
pGrimaud <pascalgrimaud@gmail.com>
2015-03-14 23:56:38 +0100

    add some scripts to install

---
+++ linuxmint/installDocker.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+# make sure the package repository is up to date
+sudo apt-get -y update
+
+# install Docker
+sudo apt-get -y install docker.io
+source /etc/bash_completion.d/docker.io
+
