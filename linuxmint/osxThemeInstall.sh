52ff4f122ff03e9ad56e8939398a5ada03f6e405
pGrimaud <pascalgrimaud@gmail.com>
2015-03-15 02:50:40 +0100

    add script : OSX-Nostalgie icons

---
+++ linuxmint/osxThemeInstall.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# from : http://gnome-look.org/content/show.php/OSX-Nostalgie?content=157390
+sudo add-apt-repository ppa:noobslab/icons -y
+sudo apt-get -y update
+sudo apt-get -y install osx-nostalgie
