376ce9f84407fd8f0bb81d43c28576b6b70d6368
pGrimaud <pascalgrimaud@gmail.com>
2015-03-17 22:39:37 +0100

    fix the cgroup-lite and apparmor package

--- linuxmint/dockerInstall.sh
+++ linuxmint/dockerInstall.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # make sure the package repository is up to date
-sudo apt-get -y update
+# On Linux Mint, the cgroup-lite and apparmor packages are not installed by default. Before Docker will work correctly, you will need to install this via:
+sudo apt-get -y update && sudo apt-get -y install cgroup-lite apparmor
 
 # install Docker
 sudo apt-get -y install docker.io
